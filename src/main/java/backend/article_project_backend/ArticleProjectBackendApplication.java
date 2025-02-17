fe863474de5d14a74b096d0d427795e006da2ac3
SoulGalaxy2312 <ngotri2312@gmail.com>
2025-02-17 08:19:39 +0700

    First commit

---
+++ src/main/java/backend/article_project_backend/ArticleProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package backend.article_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleProjectBackendApplication.class, args);
+	}
+
+}
